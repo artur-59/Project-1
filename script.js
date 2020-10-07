e3fb5ae8fc6d227b1bf43f56d96a5da2efb26e94
JoaoIsabel <joao.alves.isabel@gmail.com>
2020-10-07 17:20:22 +0100

    nothing

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+let coll = document.addEventListener("click-me");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+</script>
